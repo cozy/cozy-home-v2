73a0d3dffb3c7755093093b3c7c710587e90e0a5
Frank Rousseau <frank.rousseau@cozycloud.cc>
2015-01-07 02:17:59 +0100

    fix get icon function in client app model

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -1481,8 +1481,7 @@ module.exports = {
 var Application, client, _ref,
   __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
   __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-  __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
 client = require("../helpers/client");
 
@@ -1513,7 +1512,7 @@ module.exports = Application = (function(_super) {
     icon = iconPath || defaultIcon;
     if (icon == null) {
       attachments = this.get('_attachments');
-      if (__indexOf.call(Object.keys(attachments), 'icon.svg') >= 0) {
+      if (typeof attachments === 'object' && 'icon.svg' in attachments) {
         icon = 'icon.svg';
       }
     }
