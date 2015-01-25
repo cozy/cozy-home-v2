90f78237c6bc8e3fc16f72716516fb88fd0e0397
Frank Rousseau <frank.rousseau@cozycloud.cc>
2015-01-26 00:26:20 +0100

    make icon colors more similar (brightness, saturation)

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -583,7 +583,7 @@ exports.del = function(url, callbacks) {
 });
 
 ;require.register("helpers/color-set", function(exports, require, module) {
-module.exports = ['ead1ad', 'fbf0c2', '1e4eb1', '3cd7c3', '39a5f8', 'B4AED9', '3a6367', '85aa54', '38cc7a', '8DED2A', '8eecB9', 'bbcaA9', 'cdb19b', 'ec7e63', 'ff9c56', 'f14aa8', 'ffb1be', 'b63e57', 'ae29c2', '966f81', '40363a', 'DD99CE', 'E26987', '7cA6ff', '7b0100', 'f5dd16', 'f1fab8', 'ffbe56'];
+module.exports = ['ead1ad', 'fbf0c2', '3cd7c3', '59b5f8', 'B4AED9', '78dc9a', '8DED2A', '8eecB9', 'bbcaA9', 'cdb19b', 'ec7e63', 'ff9c56', 'ffb1be', 'DD99CE', 'E26987', '8CB1FF', 'f5dd16', 'f1fab8', 'ffbe56', '6EE1C8', 'C4BEE9', '49b5f8', 'EC8E73', '8BEE8C'];
 });
 
 ;require.register("helpers/locales", function(exports, require, module) {
