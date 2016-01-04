22b40f3d19b5db7d5f972477cdb7d7d8986e3dcd
Bruno Michel <bmichel@menfin.info>
2016-01-04 18:08:24 +0100

    Add a postinstall script for node4 compat
    Bcrypt@0.8.1 is compatible with node 0.x.y
    Bcrypt@0.8.5 is compatible with node 0.10.40+
    
    We want cozy-proxy to works on:
    
    - node 0.10.25 (the version installed by default on ubuntu)
    - node 0.10.29 (the version installed by default on debian)
    - the current stable release of node 0.10, 0.12, 4 and 5
    
    So, we have to use the postinstall feature of npm to check the version
    of node and install a version of bcrypt compatible with it.

---
+++ postinstall.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var spawn = require('child_process').spawn;
+var major = process.versions.node.split('.')[0];
+var bcrypt = 'bcrypt@0.8.5';
+
+if (major == '0') {
+  bcrypt = 'bcrypt@0.8.1';
+}
+spawn('npm', ['install', bcrypt], { stdio: 'inherit' });
